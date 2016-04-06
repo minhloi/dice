a1a0ad7722ceecee61b9b50f36cf604ed3e1339f
Minh Loi <jimmyloi92@yahoo.com>
2016-04-05 19:57:58 -0700

    Add new properties and methods to Player

--- Dice/src/Entity/Player.java
+++ Dice/src/Entity/Player.java
@@ -6,10 +6,15 @@ public class Player {
 	private String username;	
 	private int health;
 	private Dice dice;
+	// Number of times SPECIAL_ATTACK used.
+	private int numSpecialUsed;
+	private boolean blockDisabled;
 	
 	private int currentMove;
 	
+	// Some default configurations.
 	private static final int DEFAULT_HEALTH_POINT = 30;
+	private static final int MAX_SPECIAL_ALLOWED = 2;
 	
 	// Player-move definition
 	public static final int NOT_SELECT = -1;
@@ -18,9 +23,13 @@ public class Player {
 	public static final int SPECIAL_ATTACK = 2;
 	
 	public Player(int number){
+		
+		// Initialized variables
 		health = DEFAULT_HEALTH_POINT; 
 		currentMove = NOT_SELECT;
 		playerNumber = number;
+		numSpecialUsed = 0;
+		blockDisabled = false;
 		dice = new Dice();
 	}
 	
@@ -45,6 +54,17 @@ public class Player {
 		return dice;
 	}
 	
+	public boolean isBlockDisabled(){
+		return blockDisabled;
+	}
+	
+	public boolean canUseSpecial(){
+		if(numSpecialUsed >= MAX_SPECIAL_ALLOWED){
+			return false;
+		} else
+			return true;	
+	}
+		
 	public int getMove(){
 		return currentMove;
 	}
@@ -60,13 +80,39 @@ public class Player {
 			return "NOT_SELECTED";	
 	}
 	
+	// Assume that setMove is called only once in a turn,
+	// meaning once player selected a move, he cannot change.
 	public void setMove(int move){
 		currentMove = move;
+		if(currentMove == BLOCK){
+			disableBlock();
+		} else if(currentMove == SPECIAL_ATTACK){
+			incrementSpecialUsed();
+		}
 	}
 	
 	public void resetMove(){
+		
+		// If Player does not select BLOCK this turn,
+		// then enable it back for next turn.
+		if(currentMove != BLOCK){
+			enableBlock();
+		}
+		
 		currentMove = NOT_SELECT;
+		
 	}
 	
+	private void disableBlock(){
+		blockDisabled = true;
+	}
 	
+	private void enableBlock(){
+		blockDisabled = false;
+	}
+	
+	private void incrementSpecialUsed(){
+		++numSpecialUsed;
+	}
+		
 }
