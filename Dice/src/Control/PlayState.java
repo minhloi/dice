7cb4b1833c44cf28fdb27fec4d019a1e47b42762
Minh Loi <jimmyloi92@yahoo.com>
2016-04-11 12:42:25 -0700

    Modify rematch function.

--- Dice/src/Control/PlayState.java
+++ Dice/src/Control/PlayState.java
@@ -2,10 +2,14 @@ package Control;
 
 import java.util.Scanner;
 
+import Entity.Player;
+
 public class PlayState extends State{
 
 	private GameController gameController;	
 	private Match currentMatch;
+	private Player player1;
+	private Player player2;
 	
 	public PlayState(GameController controller, Scanner scanner){
 		
@@ -15,15 +19,27 @@ public class PlayState extends State{
 	}
 	
 	public void createNewMatch(){
-		currentMatch = new Match(scanner);
+		
+		// Create new players.
+		player1 = new Player(1);
+		player2 = new Player(2);
+		
+		// Create new match.
+		currentMatch = new Match(player1, player2, scanner);
+	
 	}
 	
 	public void rematch(){
-			
+		
+		// If rematch is selected, create a new match
+		// but use current players' information.
+		currentMatch = new Match(player1, player2, scanner);
 	}
 	
 	public void print(){
+		
 		currentMatch.begin();
+	
 	};
 	
 	
