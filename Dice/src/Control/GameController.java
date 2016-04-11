b8f0757b492218ddcfa7ba1ea8bd6a5cdcd1be7f
Minh Loi <jimmyloi92@yahoo.com>
2016-04-11 13:03:41 -0700

    Add new state to list.

--- Dice/src/Control/GameController.java
+++ Dice/src/Control/GameController.java
@@ -11,16 +11,18 @@ public class GameController {
 	public static final int MENU_STATE = 0;
 	public static final int PLAY_STATE = 1;
 	public static final int VIEW_RANK_STATE = 2;
+	public static final int MATCH_END_MENU_STATE = 3; // Menu displayed when a match ended
 	
 	public GameController(){
 		
 		Scanner scanner = new Scanner(System.in);
 		
-		stateLength = 3;
+		stateLength = 4;
 		stateList = new State[stateLength];
 		stateList[MENU_STATE] = new MenuState(this, scanner);
 		stateList[PLAY_STATE] = new PlayState(this, scanner);
 		stateList[VIEW_RANK_STATE] = new ViewRankState(this, scanner);
+		stateList[MATCH_END_MENU_STATE] = new MatchEndMenuState(this, scanner);
 		
 		// Default is Menu
 		currentState = MENU_STATE;
