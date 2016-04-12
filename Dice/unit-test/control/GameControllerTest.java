438f515a3d469b3f0af712220e29cabb8a7acb6f
Minh Loi <jimmyloi92@yahoo.com>
2016-04-12 14:07:34 -0700

    Add GameControllerTest

---
+++ Dice/unit-test/control/GameControllerTest.java
@@ -0,0 +1,34 @@
+package control;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameControllerTest {
+
+	@Test
+	public void testGameController() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInit() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetState() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetState() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExitGame() {
+		//fail("Not yet implemented");
+	}
+
+}
