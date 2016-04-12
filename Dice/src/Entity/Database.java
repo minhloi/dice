abb44c1503a64818198bc169ed02e9056e7e45c5
Minh Loi <jimmyloi92@yahoo.com>
2016-04-11 18:12:56 -0700

    Add more functionality to Database class.

--- Dice/src/Entity/Database.java
+++ Dice/src/Entity/Database.java
@@ -4,43 +4,90 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Vector;
+import java.util.Iterator;
 
 public class Database {
 
 	private BufferedReader bufferedReader;
 	private FileReader fileReader;
-	private Vector<PlayerScore> data;
-	private static final String FILE_NAME = "game.txt";
+	
+	private Vector<PlayerScore> gameData;
+	private static final String FILE_NAME = "database.txt";
 	
 	public Database(){
 		
-		data = new Vector();
+		gameData = new Vector();
 		
 	}
 	
 	public void loadData(){
 		
 		try {
-		
-			BufferedReader fileIn = new BufferedReader(new FileReader(FILE_NAME));
-		
+			BufferedReader fileRead = new BufferedReader(new FileReader(FILE_NAME));
 		} catch (FileNotFoundException e) {
-			
-			
 			
 		}
 	
 	}
 	
+	// It is better to dump all data to a file
+	// because we don't know which line has been changed.
 	public void saveData(){
+		
+		PlayerScore target = null;
+		Iterator<PlayerScore> iterator =  gameData.iterator();
+		
+		while(iterator.hasNext()){
 			
-	
+
+		}
+		
 	}
 	
-	public void get(){
+	// TODO: Use BubbleSort to sort players by difference of wins and losses
+	public void sortByDifference(){
+				
 		
-		
+	}
 	
+	public void incrementWinByName(String username){
+		
+		PlayerScore target = getPlayerScoreByName(username);
+		
+		if(target == null){
+			target = new PlayerScore(username, 0, 0);
+		} 
+		
+		target.incrementWins();		
+				
+	}
+	
+	public void incrementLossByName(String username){
+		
+		PlayerScore target = getPlayerScoreByName(username);
+		
+		if(target == null){
+			target = new PlayerScore(username, 0, 0);
+		} 
+		
+		target.incrementLosses();
+		
+	}
+	
+	private PlayerScore getPlayerScoreByName(String username){
+		
+		PlayerScore target = null;
+		Iterator<PlayerScore> iterator =  gameData.iterator();
+		
+		while(iterator.hasNext()){
+			PlayerScore player = iterator.next();
+			if(username.equals(player.getUsername())){
+				 target = player;
+			}
+		}
+		
+		return target;
+		
 	}
 	
 }
