a6082ca27afd05139898d0430a3f9731e9a50157
Minh Loi <jimmyloi92@yahoo.com>
2016-04-11 18:56:28 -0700

    Add database property

--- Dice/src/Control/ViewRankState.java
+++ Dice/src/Control/ViewRankState.java
@@ -2,20 +2,23 @@ package Control;
 
 import java.util.Scanner;
 
+import Entity.Database;
+
 public class ViewRankState extends State{
 	
 	private GameController gameController;
+	private Database database;
 	
-	public ViewRankState(GameController controller, Scanner scanner){
+	public ViewRankState(GameController controller, Scanner scanner, Database database){
 		
 		this.gameController = controller;
 		this.scanner = scanner;
+		this.database = database;
 		
+	}
+		
+	public void print(){
+					
 	}
 	
-	public void print(){
-		
-		
-	}
-
 }
