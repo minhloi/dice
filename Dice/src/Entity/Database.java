b6adf29f9ea84794390ffe7290f4759b36c2c15b
Minh Loi <jimmyloi92@yahoo.com>
2016-04-11 18:56:47 -0700

    Complete database functionality

--- Dice/src/Entity/Database.java
+++ Dice/src/Entity/Database.java
@@ -1,46 +1,76 @@
 package Entity;
 
-import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Vector;
 import java.util.Iterator;
 
 public class Database {
 
-	private BufferedReader bufferedReader;
-	private FileReader fileReader;
-	
+    FileOutputStream fileOutput;
+    ObjectOutputStream outStream;
+    
+    FileInputStream fileInput;
+    ObjectInputStream inStream;
+    
 	private Vector<PlayerScore> gameData;
 	private static final String FILE_NAME = "database.txt";
 	
 	public Database(){
-		
-		gameData = new Vector();
-		
 	}
 	
+	@SuppressWarnings("unchecked")
 	public void loadData(){
 		
 		try {
-			BufferedReader fileRead = new BufferedReader(new FileReader(FILE_NAME));
-		} catch (FileNotFoundException e) {
 			
+			fileInput = new FileInputStream(FILE_NAME);
+			inStream = new ObjectInputStream(fileInput);
+			
+			Object data = inStream.readObject();
+			if( data instanceof Vector<?>){
+				gameData = (Vector<PlayerScore>) data;
+			}
+	
+		} catch (FileNotFoundException e) {
+		
+			gameData = new Vector();
+				
+		} catch (ClassNotFoundException e) {
+		
+			e.printStackTrace();
+			
+		} catch (IOException e) {
+		
+			e.printStackTrace();
+		
 		}
 	
 	}
 	
-	// It is better to dump all data to a file
-	// because we don't know which line has been changed.
+	// It is better to dump all objects to a file
+	// because we don't know which objects have been changed.
 	public void saveData(){
 		
-		PlayerScore target = null;
-		Iterator<PlayerScore> iterator =  gameData.iterator();
+		try {
 		
-		while(iterator.hasNext()){
+			fileOutput = new FileOutputStream(FILE_NAME, false);
+			outStream = new ObjectOutputStream(fileOutput);
 			
-
+			outStream.writeObject(gameData);
+						
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
+		
 		
 	}
 	
