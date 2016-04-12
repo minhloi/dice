ea32441015f6e65fa43f4364c27b293732e43379
Minh Loi <jimmyloi92@yahoo.com>
2016-04-11 20:45:00 -0700

    Complete viewRankState functionality

--- Dice/src/Control/ViewRankState.java
+++ Dice/src/Control/ViewRankState.java
@@ -1,13 +1,22 @@
 package Control;
 
+import java.util.Iterator;
 import java.util.Scanner;
+import java.util.Vector;
 
 import Entity.Database;
+import Entity.PlayerScore;
 
 public class ViewRankState extends State{
 	
 	private GameController gameController;
 	private Database database;
+	
+	private String[] menuList; 
+	private int menuLength;
+	
+	private static final int BACK_TO_MAIN_MENU = 0;
+	private static final int EXIT = 1;
 	
 	public ViewRankState(GameController controller, Scanner scanner, Database database){
 		
@@ -15,10 +24,82 @@ public class ViewRankState extends State{
 		this.scanner = scanner;
 		this.database = database;
 		
+		menuLength = 2;
+		menuList = new String[menuLength];
+		menuList[BACK_TO_MAIN_MENU] = "Back to main menu";
+		menuList[EXIT] = "Exit";
+		
 	}
 		
 	public void print(){
-					
+
+		// Sort data by difference of wins and losses.
+		database.sortByDifference();
+		Vector<PlayerScore> data = database.getData();
+		
+		int size = data.size();
+		
+		if(size > 0){
+		
+			Iterator<PlayerScore> iterator =  data.iterator();
+			System.out.println("Viewing ranks of players in order of DIFF.");
+			
+			System.out.printf("%-25s%-10s%-10s%-10s\n", "Username", "Wins", "Losses", "DIFF");
+			System.out.println("-----------------------------------------------------------------------------");
+			
+			while(iterator.hasNext()){
+				PlayerScore player = iterator.next();
+				int numOfWins = player.getNumOfWins();
+				int numOfLosses = player.getNumOfLosses();
+				int diff = numOfWins - numOfLosses;
+				System.out.printf("%-25s%-10s%-10s%-10s\n", player.getUsername(), numOfWins, numOfLosses, diff);
+				
+			}
+			
+		} else {
+			System.out.println("No records in database.");
+		}
+				
+		System.out.println();
+		printMenu();
+	
 	}
 	
+	// Print all menu options.
+	public void printMenu(){
+	
+		System.out.print("Menu options: \n");
+		for(int i = 0; i < menuLength; i++){
+			System.out.print(i + ". " + menuList[i]+ "\n");
+		}
+				
+		System.out.print("Please select menu: ");
+		
+		// Read input
+		int selectedOption = scanner.nextInt();
+		
+		System.out.print("\n");
+		
+		// Route to corrected state.
+		route(selectedOption);
+			
+	}
+	
+	private void route(int selectedOption){
+		
+		if(selectedOption == BACK_TO_MAIN_MENU){
+			
+			// Begin to render menuState.
+			gameController.setState(GameController.MENU_STATE);
+								
+		}
+				
+		else if(selectedOption == EXIT){
+			
+			gameController.exitGame();
+			
+		}
+		
+	}
+
 }
