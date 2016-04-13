af73536a3ecaeb2dedfe1bfe942614d277b09b7c
Minh Loi <jimmyloi92@yahoo.com>
2016-04-13 12:25:20 -0700

    Create MatchTest to test Match

---
+++ Dice/unit-test/control/MatchTest.java
@@ -0,0 +1,29 @@
+package control;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MatchTest {
+
+	@Test
+	public void testMatch() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBegin() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNextTurn() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testResetPhase() {
+		//fail("Not yet implemented");
+	}
+
+}
