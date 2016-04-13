cb590c3bf886973d7f38447ec1e7b45c3db5f60f
Minh Loi <jimmyloi92@yahoo.com>
2016-04-13 12:29:13 -0700

    Create PlayerScoreTest to PlayerScore

---
+++ Dice/unit-test/entity/PlayerScoreTest.java
@@ -0,0 +1,44 @@
+package entity;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlayerScoreTest {
+
+	@Test
+	public void testPlayerScore() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUsername() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNumOfWins() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNumOfLosses() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDifference() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIncrementWins() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIncrementLosses() {
+		//fail("Not yet implemented");
+	}
+
+}
