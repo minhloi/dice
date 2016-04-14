e01bcb7028c7cb536b6c9fba7f54aff9072e6218
Minh Loi <jimmyloi92@yahoo.com>
2016-04-13 19:40:59 -0700

    Update test cases for GameController

--- Dice/unit-test/control/GameControllerTest.java
+++ Dice/unit-test/control/GameControllerTest.java
@@ -2,33 +2,55 @@ package control;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 import org.junit.Test;
 
 public class GameControllerTest {
 
 	@Test
 	public void testGameController() {
-		//fail("Not yet implemented");
-	}
-
-	@Test
-	public void testInit() {
-		//fail("Not yet implemented");
+		
+		// Test if a GameController object is created.
+		GameController gameController = new GameController();	
+		assertNotNull(gameController);
 	}
 
 	@Test
 	public void testGetState() {
-		//fail("Not yet implemented");
+		GameController gameController = new GameController();	
+		
+		// Test if getState returns a correct object.
+		assertTrue(gameController.getState(GameController.MENU_STATE) instanceof MenuState);
+		assertTrue(gameController.getState(GameController.PLAY_STATE) instanceof PlayState);
+		assertTrue(gameController.getState(GameController.VIEW_RANK_STATE) instanceof ViewRankState);
+		assertTrue(gameController.getState(GameController.MATCH_END_MENU_STATE) instanceof MatchEndMenuState);
 	}
 
 	@Test
 	public void testSetState() {
-		//fail("Not yet implemented");
+		
+		GameController gameController = new GameController();	
+		assertNotNull(gameController);
+		
 	}
 
 	@Test
 	public void testExitGame() {
-		//fail("Not yet implemented");
+		
+		ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+		
+		GameController gameController = new GameController();
+		
+		gameController.exitGame();
+		
+		// The game exited sucessfully.
+		assertEquals("Exit!", outContent.toString());
+		
 	}
 
 }
