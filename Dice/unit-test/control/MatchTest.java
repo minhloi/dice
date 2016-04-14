7eb0d8dc8bc40407d49e12fe239792a9ea5aeeaf
Minh Loi <jimmyloi92@yahoo.com>
2016-04-13 19:28:51 -0700

    Add comments

--- Dice/unit-test/control/MatchTest.java
+++ Dice/unit-test/control/MatchTest.java
@@ -2,28 +2,29 @@ package control;
 
 import static org.junit.Assert.*;
 
+import java.util.Scanner;
+
 import org.junit.Test;
+
+import entity.Database;
+import entity.Player;
 
 public class MatchTest {
 
 	@Test
 	public void testMatch() {
-		//fail("Not yet implemented");
-	}
+		
+		Player player1 = new Player(1, "Guest");
+		Player player2 = new Player(2, "Guest2");
 
-	@Test
-	public void testBegin() {
-		//fail("Not yet implemented");
-	}
-
-	@Test
-	public void testNextTurn() {
-		//fail("Not yet implemented");
-	}
-
-	@Test
-	public void testResetPhase() {
-		//fail("Not yet implemented");
+		GameController controller = new GameController();
+		Scanner scanner = new Scanner(System.in);
+		Database database = new Database();
+		
+		// Test if a Match object is created
+		Match match = new Match(player1, player2, controller, scanner, database);
+		assertNotNull(match);
+	
 	}
 
 }
