0d72f6b4726f858fc207130f42a3892cb04edc04
Minh Loi <jimmyloi92@yahoo.com>
2016-04-13 19:54:42 -0700

    Add commments

--- Dice/unit-test/entity/PlayerScoreTest.java
+++ Dice/unit-test/entity/PlayerScoreTest.java
@@ -8,30 +8,38 @@ public class PlayerScoreTest {
 
 	@Test
 	public void testPlayerScore() {
+		// Test if a PlayerScore object is created sucessfully.
 		PlayerScore newPlayer = new PlayerScore("guest");
 		assertNotNull(newPlayer);
 	}
 
 	@Test
 	public void testGetUsername() {
+		// Test if user name is corrected returned.
 		PlayerScore newPlayer = new PlayerScore("guest");
 		assertEquals("guest", newPlayer.getUsername());
 	}
 
 	@Test
 	public void testGetNumOfWins() {
+
+		// Test if numOfWins is corrected returned.
 		PlayerScore newPlayer = new PlayerScore("guest");
 		assertEquals(0, newPlayer.getNumOfWins());
 	}
 
 	@Test
 	public void testGetNumOfLosses() {
+
+		// Test if numOfLoss is corrected returned.
 		PlayerScore newPlayer = new PlayerScore("guest");
 		assertEquals(0, newPlayer.getNumOfLosses());
 	}
 
 	@Test
 	public void testGetDifference() {
+		
+		// Test if difference is calculated correctly.
 		PlayerScore newPlayer = new PlayerScore("guest");
 		newPlayer.incrementWins();
 		newPlayer.incrementWins();
@@ -41,6 +49,8 @@ public class PlayerScoreTest {
 
 	@Test
 	public void testIncrementWins() {
+		
+		// Test if numOfWins incremented
 		PlayerScore newPlayer = new PlayerScore("guest");
 		newPlayer.incrementWins();
 		assertEquals(1, newPlayer.getNumOfWins());
@@ -48,6 +58,8 @@ public class PlayerScoreTest {
 
 	@Test
 	public void testIncrementLosses() {
+
+		// Test if numOfLosses incremented
 		PlayerScore newPlayer = new PlayerScore("guest");
 		newPlayer.incrementLosses();
 		assertEquals(1, newPlayer.getNumOfLosses());
