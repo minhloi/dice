4b12d4580b0bfc346e07b971a83624420bcfaf5c
denriquez <denriquez@SLICK-PC>
2016-04-15 19:47:49 -0700

    Added documentation for PlayerScore class

--- Dice/src/entity/PlayerScore.java
+++ Dice/src/entity/PlayerScore.java
@@ -3,8 +3,7 @@ package entity;
 import java.io.Serializable;
 
 /**
- * The PlayerScore class is use to get players name and store data
- * of correct player.
+ * PlayerScore class - Use to get player's name and store data of correct player.
  * 
  * @author Thien Duc Phung
  * @author Minh Loi
@@ -12,39 +11,69 @@ import java.io.Serializable;
  * @author Brett Bauman
  * @author Tanner Siffren
  */
-
 public class PlayerScore implements Serializable {
 	
 	private String playerName;
 	private int numOfWins;
 	private int numOfLosses;
 	
+	/**
+	 * Constructor - Set the player's username
+	 * 
+	 * @param username The player's username
+	 */
 	public PlayerScore(String username){
 		playerName = username;
 		this.numOfWins = 0;
 		this.numOfLosses = 0;
 	}
-		
+	
+	/**
+	 * getUsername - Get the player's username
+	 * 
+	 * @return Returns the player's username
+	 */
 	public String getUsername(){
 		return playerName;
 	}
 	
+	/**
+	 * getNumOfWins - Get the number of wins
+	 * 
+	 * @return Returns the number of wins
+	 */
 	public int getNumOfWins(){
 		return numOfWins;		
 	}
 	
+	/**
+	 * getNumOfLosses - Get the number of losses
+	 * 
+	 * @return Returns the number of losses
+	 */
 	public int getNumOfLosses(){
 		return numOfLosses;		
 	}
 	
+	/**
+	 * getDifference - Get the difference between the number of wins and losses
+	 * 
+	 * @return Returns the difference between the number of wins and losses
+	 */
 	public int getDifference(){
 		return (numOfWins - numOfLosses);
 	}
 	
+	/**
+	 * incrementWins - Increment the number of wins
+	 */
 	public void incrementWins(){
 		numOfWins++;
 	}
 	
+	/**
+	 * incrementLosses - Increment the number of losses
+	 */
 	public void incrementLosses(){
 		numOfLosses++;
 	}
