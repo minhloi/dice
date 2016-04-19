a9ccda1bca61e4799d6f43b33cdd5c26949b3796
Minh Loi <jimmyloi92@yahoo.com>
2016-04-18 21:51:47 -0700

    Update GUI core

---
+++ Dice/src/boundary/HealthBar.java
@@ -0,0 +1,19 @@
+package boundary;
+
+public class HealthBar extends GameObject {
+	
+	private static final int BAR_WIDTH = 500;
+	private static final int BAR_HEIGHT = 50;
+	
+	
+	public Healthbar (){
+		
+		
+	}
+	
+	public void setHealth(int health){
+		
+		
+	}
+	
+}
--- Dice/src/boundary/PlayerObject.java
+++ Dice/src/boundary/PlayerObject.java
@@ -23,7 +23,7 @@ public class PlayerObject extends GameObject{
 	public static final int PLAYER2_DEFAULT_POSITION_X = GameCanvas.WIDTH - WIDTH;
 	public static final int PLAYER2_DEFAULT_POSITION_Y = 210;
 	public static final int FRONT_OF_PLAYER1 = 80;
-	public static final int FRONT_OF_PLAYER2 = 480;
+	public static final int FRONT_OF_PLAYER2 = 460;
 	
 	// There are a total of 8 states in one running step
 	public static final int STATES_PER_STEP = 8;
@@ -36,7 +36,7 @@ public class PlayerObject extends GameObject{
 	public static final int STATE_CHANGE_SPEED = STEP_PER_PX / STATES_PER_STEP;
 	
 	// Running speed: How many pixels it runs per 25 ms (The game loop runs every 25 ms).
-	public static final int RUNNING_SPEED = 6;
+	public static final int RUNNING_SPEED = 9;
 	
 	public static final int ATTACK_STATE_NUM = 7;
 	
