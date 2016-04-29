0a1408a0ef254ba784c1d61c3c8fba59bd23a071
Minh Loi <jimmyloi92@yahoo.com>
2016-04-29 13:26:46 -0700

    Add new test cases for DiceObject

--- Dice/unit-test/boundary/DiceObjectTest.java
+++ Dice/unit-test/boundary/DiceObjectTest.java
@@ -14,7 +14,40 @@ public class DiceObjectTest {
 
 	@Test
 	public void testSetImageByDiceNum() {
-		//fail("Not yet implemented");
+		DiceObject diceObject = new DiceObject(0, 0);
+		
+		diceObject.setImageByDiceNum(2);
+		assertNotNull(diceObject);
+	
 	}
+	
+	@Test
+	public void testSetPosition(){
+		DiceObject diceObject = new DiceObject(0, 0);
+		
+		diceObject.setPosition(15, 30);
+		assertEquals(15, diceObject.getPositionX());
+		assertEquals(30, diceObject.getPositionY());
+		
+	}
+	
+	@Test
+	public void testSetPositionX(){
+		DiceObject diceObject = new DiceObject(0, 0);
+		
+		diceObject.setPositionX(15);
+		assertEquals(15, diceObject.getPositionX());
+		
+	}
+	
+	@Test
+	public void testSetPositionY(){
+		DiceObject diceObject = new DiceObject(0, 0);
+		
+		diceObject.setPositionY(35);
+		assertEquals(35, diceObject.getPositionX());
+		
+	}
+	
 
 }
