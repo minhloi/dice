25aa8895d10159838473891cf0488839d0525eb3
tanner-siffren <tsiffren@gmail.com>
2016-04-29 14:25:47 -0700

    Update ProfileTextField.java

--- Dice/src/boundary/ProfileTextField.java
+++ Dice/src/boundary/ProfileTextField.java
@@ -28,17 +28,34 @@ public class ProfileTextField extends GameObject {
 	
 	private static final int MARGIN_TOP = 80;
 	
+	/**
+	 * The defaukt position of the profile text field
+	 * 
+	 * */
 	public ProfileTextField(){
 		setPosition(DEFAULT_POSITION_X, DEFAULT_POSITION_Y);
 		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);		
 		
 	}
+	
+	/**
+	 * Sets the position of the text field for the profiles
+	 * 
+	 * @param positionX - the X position of the text field
+	 * @param positionY - the Y position of the text field
+	 * */
 	public ProfileTextField(int positionX, int postionY){
 		setPosition(positionX, positionY);
 		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);		
 		
 	}
 	
+	/**
+	 * Informs the user to enter a username for either player 1 and 2 
+	 * 
+	 * @param playernumber - shows the user which player 1 or 2 needs to enter username
+	 * @param username - The name the gets entered for the desired player
+	 * */
 	public void drawUsername(int playerNumber, String username){
 		
 		Graphics2D graphics = image.createGraphics();
