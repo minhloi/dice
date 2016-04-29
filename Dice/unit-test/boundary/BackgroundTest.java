fce5ab0b59700fb7f7fc85103df59237d6f10a59
Minh Loi <jimmyloi92@yahoo.com>
2016-04-29 13:21:52 -0700

    Add new test cases for Background

--- Dice/unit-test/boundary/BackgroundTest.java
+++ Dice/unit-test/boundary/BackgroundTest.java
@@ -2,6 +2,11 @@ package boundary;
 
 import static org.junit.Assert.*;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 import org.junit.Test;
 
 public class BackgroundTest {
@@ -13,8 +18,40 @@ public class BackgroundTest {
 	}
 
 	@Test
-	public void testSetBackground() {
-		//fail("Not yet implemented");
+	public void testSetPosition() {
+		Background background = new Background("play_background.png");
+		background.setPosition(10, 20);
+		
+		assertEquals(10, background.getPositionX());
+		assertEquals(20, background.getPositionY());
+		
 	}
-
+	
+	@Test
+	public void testSetPositionX() {
+		Background background = new Background("play_background.png");
+		background.setPositionX(10);
+		
+		assertEquals(10, background.getPositionX());
+		
+	}
+	
+	@Test
+	public void testSetPositionY() {
+		Background background = new Background("play_background.png");
+		background.setPositionY(20);
+		
+		assertEquals(20, background.getPositionY());
+		
+	}
+	
+	@Test
+	public void testSetBackground(){
+		
+		Background background = new Background("play_background.png");
+		background.setBackground("main_background.png");
+		
+		assertNotNull(background.getImage());
+	}
+	
 }
