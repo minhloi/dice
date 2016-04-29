a5ee97e3396e06c8014d759ec9b175236bbfd1dc
denriquez <denriquez@SLICK-PC>
2016-04-29 16:39:55 -0700

    Added documentation to TurnInfo class

--- Dice/src/entity/TurnInfo.java
+++ Dice/src/entity/TurnInfo.java
@@ -1,5 +1,13 @@
 package entity;
-
+/**
+ * TurnInfo class - Set and return each turn's winner, blocking and move info
+ * 
+ * @author Thien Duc Phung
+ * @author Minh Loi
+ * @author Daniel Enriquez
+ * @author Brett Bauman
+ * @author Tanner Siffren
+ */
 public class TurnInfo {
 	
 	private int currentMove;
@@ -7,6 +15,9 @@ public class TurnInfo {
 	private boolean turnWinner;
 	private boolean moveAlreadySet;
 	
+	/**
+	 * Set blockDisabled, turnWinner, and moveAlreadySet to false. Also, set currentMove to not selected
+	 */
 	public TurnInfo(){
 		blockDisabled = false;
 		turnWinner = false;
@@ -14,22 +25,45 @@ public class TurnInfo {
 		currentMove = Player.NOT_SELECT;
 	}
 	
+	/**
+	 * isBlockDisabled - Return whether or not block is disabled 
+	 * 
+	 * @return Returns true if block is disabled and false if it isn't
+	 */
 	public boolean isBlockDisabled(){
 		return blockDisabled;
 	}
 	
+	/**
+	 * getMove - Get the current move
+	 * 
+	 * @return Returns the current move
+	 */
 	public int getMove(){
 		return currentMove;
 	}
 	
+	/**
+	 * setTurnWinner - Set turnWinner to true
+	 */
 	public void setTurnWinner(){
 		turnWinner = true;
 	}
 	
+	/**
+	 * isTurnWinner - Check if player is turn's winner
+	 * 
+	 * @return Returns true if a player is the turn's winner or false otherwise
+	 */
 	public boolean isTurnWinner(){
 		return turnWinner;
 	}
 	
+	/**
+	 * getMoveInString - Match string to either attack, block, and special attack
+	 * 
+	 * @return Returns string of the appropriate move
+	 */
 	public String getMoveInString(){
 		String moveString;
 		switch (currentMove){
@@ -48,6 +82,12 @@ public class TurnInfo {
 		return moveString;
 	}
 	
+	/**
+	 * setMove - Set the current move
+	 * 
+	 * @param move Move identification integer
+	 * @throws Exception Move has already been set by player
+	 */
 	public void setMove(int move) throws Exception{
 		
 		if(moveAlreadySet == true ){
@@ -57,10 +97,16 @@ public class TurnInfo {
 		}
 	}
 	
+	/**
+	 * disableBlock - Block has been disabled
+	 */
 	public void disableBlock(){
 		blockDisabled = true;
 	}
 	
+	/**
+	 * enableBlock - block has been enabled
+	 */
 	public void enableBlock(){
 		blockDisabled = false;
 	}
