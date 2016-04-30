19f614017e75328e7ef56b02e392e71d181bd7a2
Minh Loi <jimmyloi92@yahoo.com>
2016-04-29 21:07:32 -0700

    Fix some documentation

--- Dice/src/boundary/Background.java
+++ Dice/src/boundary/Background.java
@@ -1,7 +1,7 @@
 package boundary;
 
 /**
- * The Background class is use for adding image as a background
+ * The Background class is used to add an image as a background
  * 
  * @author Thien Duc Phung
  * @author Minh Loi
@@ -11,11 +11,19 @@ package boundary;
  */
 
 public class Background extends GameObject {
-
+	
+	/**
+	 * The Background constructor initializes image by filename
+	 * @param filename the filename of the image
+	 */
 	public Background(String filename){
 		setImageByPath("/background/" + filename);
 	}
 	
+	/**
+	 * Set background image by filename
+	 * @param filename the filename of the image
+	 */
 	public void setBackground(String filename){
 		setImageByPath("/background/" + filename);
 	}
--- Dice/src/boundary/DiceObject.java
+++ Dice/src/boundary/DiceObject.java
@@ -1,7 +1,8 @@
 package boundary;
 
 /**
- * The DiceObject class is setting dice location in the game panel
+ * The DiceObject class renders dice image
+ * and sets location in the panel
  * 
  * @author Thien Duc Phung
  * @author Minh Loi
@@ -18,20 +19,30 @@ public class DiceObject extends GameObject {
 	public static final int HEIGHT = 60;
 	public static final int WIDTH = 60;
 	
-	// Position variables of the dice panel in game panel
+	// Position of the dice objects relative to the game panel
 	public static final int DICE1_POSITION_X = Panel.PANEL_1_POSITION_X + Panel.WIDTH - WIDTH - 10;
 	public static final int DICE1_POSITION_Y = Panel.PANEL_1_POSITION_Y + 10;
 	public static final int DICE2_POSITION_X = Panel.PANEL_2_POSITION_X + Panel.WIDTH - WIDTH - 10;
 	public static final int DICE2_POSITION_Y = Panel.PANEL_2_POSITION_Y + 10;
 		
-	// Create the dice panel for dice image
+
+	/**
+	 * The DiceObject constructor initializes position of the dice
+	 * and locattion of resource folder
+	 * 
+	 * @param positionX position of the dice in the x-axis
+	 * @param positionY position of the dice in the y-axis
+	 */
 	public DiceObject(int positionX, int positionY){
 		
 		setPosition(positionX, positionY);
 		this.resourceFolder = "/dice/";
 	}
 	
-	// Assign dice image into dice panel
+	/**
+	 * Render dice image by its number
+	 * @param diceNum the dice number
+	 */
 	public void setImageByDiceNum(int diceNum){
 		setImageByPath(resourceFolder + diceNum + ".png");		
 	}
